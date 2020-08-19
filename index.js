2a6cc5ffe44e614c529a31f114d7eca0226ca3bc
crisp-tuple-3290 <faustorebeccabsit@gmail.com>
2020-08-19 18:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  var string = `I love you, Grandma. `
+  if string.toUpperCase()===string{
+    return `YES INDEED`
+  }
+  else if (string.toLowerCase()===string) {
+    return `I can't hear you!`
+  }
+  else if (string===`I love you, Grandma.`) {
+    return `I love you too.`
+
+  }
+}
